57a43952988df3644fa9987498406adb42173906
isnendyankp <isnendyankp@gmail.com>
2024-02-16 22:54:41 +0700

    Add id, name, and phone columns to Customer model

--- flaskzooapi/app/models/customers.py
+++ flaskzooapi/app/models/customers.py
@@ -2,3 +2,6 @@ from app.utils.database import db
 
 
 class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(100), nullable=True)
