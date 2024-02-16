7e017be43ee2f59b326648976bdd91898cace2d3
isnendyankp <isnendyankp@gmail.com>
2024-02-16 23:10:30 +0700

    Add try exception

--- flaskzooapi/app/route/customer_route.py
+++ flaskzooapi/app/route/customer_route.py
@@ -7,12 +7,15 @@ customers_blueprint = Blueprint('customers_endpoint', __name__)
 # this is the route for the customer endpoint
 @customers_blueprint.route("/", methods=["GET"])
 def create_customers():
-    # create a new customer
-    customers = Customer()
-    # set the customer attributes
-    customers.name = "John Doe"
-    customers.phone = "123-456-7890"
-    # add the customer to the database
-    db.session.add(customers)
-    db.session.commit()
-    return 'Customer created successfully!', 200
+    try:
+        # create a new customer
+        customers = Customer()
+        # set the customer attributes
+        customers.name = "John Doe"
+        customers.phone = "123-456-7890"
+        # add the customer to the database
+        db.session.add(customers)
+        db.session.commit()
+        return 'Customer created successfully!', 200
+    except Exception as e:
+        return e, 500
