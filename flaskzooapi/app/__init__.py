b38d50166c618e1106408ff0c65074f1c0d069de
isnendyankp <isnendyankp@gmail.com>
2024-02-16 22:34:10 +0700

    Add database configuration to .env file and update app initialization

--- flaskzooapi/app/__init__.py
+++ flaskzooapi/app/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
 from app.route import pokemon_route
+import os
 
 app = Flask(__name__)
+
+DATABASE_TYPE = os.getenv("DATABASE_TYPE")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+app.config["SQLALCHEMY_DATABASE_URI"] = f""
 
 app.register_blueprint(pokemon_route.pokemon_blueprint, url_prefix="/pokemons")
