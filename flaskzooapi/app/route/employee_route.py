acf880b218820cef8848c3ed27a2ef3a413a9efb
isnendyankp <isnendyankp@gmail.com>
2024-02-16 23:42:07 +0700

    cr8 employee_route

---
+++ flaskzooapi/app/route/employee_route.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, jsonify
+
+employee_blueprint = Blueprint('employee_endpoint', __name__)
+
+# Method GET
+@employee_blueprint.route("/", methods=["GET"])
+def get_employees():
+    return jsonify('Jenis-jenis Employees :', [
+        {
+            'id': 1,
+            'name': 'Budi',
+            'position': 'Manager',
+            'department': 'IT'
+        },
+        {
+            'id': 2,
+            'name': 'Andi',
+            'position': 'Staff',
+            'department': 'IT'
+        },
+        {
+            'id': 3,
+            'name': 'Cindy',
+            'position': 'Manager',
+            'department': 'HRD'
+        }
+    ])
+
+# Method POST
+@employee_blueprint.route("/", methods=["POST"])
+def create_employee():
+    return 'berhasil', 200
+
+# Method PUT
+@employee_blueprint.route("/", methods=["PUT"])
+def update_employee():
+    return 'berhasil', 200
+
+# Method DELETE
+@employee_blueprint.route("/", methods=["DELETE"])
+def delete_employee():
+    return 'berhasil', 200 
