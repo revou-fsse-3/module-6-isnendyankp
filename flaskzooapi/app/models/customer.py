4f7013d619f6fc519c427ef56f9966abbf788a49
isnendyankp <isnendyankp@gmail.com>
2024-02-18 19:34:00 +0700

    Add as_dict method to Customer model

--- flaskzooapi/app/models/customer.py
+++ flaskzooapi/app/models/customer.py
@@ -5,3 +5,11 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(100), nullable=True)
+
+    # method to return the customer as a dictionary
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "phone": self.phone
+        }
