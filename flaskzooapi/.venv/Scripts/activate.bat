35b83d2ad0e90bdc38ab0dc8c8563e596002aabb
isnendyankp <isnendyankp@gmail.com>
2024-02-23 14:50:36 +0700

    add Test for the root endpoint

--- flaskzooapi/.venv/Scripts/activate.bat
+++ flaskzooapi/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=G:\PROGRAMMER\Revoyu\Weekly\Modul 6\Week 15\Assignment\module-6-isnendyankp\flaskzooapi\.venv
+set VIRTUAL_ENV=G:\PROGRAMMER\Revoyu\Weekly\Modul 6\Week 16\Assignment\module-6-isnendyankp\flaskzooapi\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
---
+++ flaskzooapi/tests/test_app.py
@@ -0,0 +1,14 @@
+import pytest
+from app import app, db
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+# Test for the root endpoint
+def test_database_connection():
+    with app.app_context():
+        db.create_all()
+        assert db.engine.url.database == 'postgres'
